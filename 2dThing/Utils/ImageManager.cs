674f8b300bc3f0a149df41fd43b355f54b6af946
Cyril Paulus <paulus.cyril@gmail.com>
2011-08-05 00:05:49 +0200

    ImageManager code cleaning

--- 2dThing/Utils/ImageManager.cs
+++ 2dThing/Utils/ImageManager.cs
@@ -2,36 +2,29 @@ using System;
 using System.Collections.Generic;
 using SFML.Graphics;
 
-namespace _2dThing
-{
-	public class ImageManager
-	{
-		Dictionary<String, Image> imageSet;
+namespace _2dThing {
+	public class ImageManager {
+		private Dictionary<String, Image> imageSet;
 		
-		public ImageManager ()			
-		{
+		public ImageManager() {
 			imageSet = new Dictionary<string, Image>();
 		}
 		
-		public void Load(String name, String path){
-			if(!imageSet.ContainsKey("name")){
+		public void Load(String name, String path) {
+			if (!imageSet.ContainsKey("name")) {
 				Image myImage = new Image(path);
 				imageSet.Add(name, myImage);
 			}
 		}
 		
-		
-		public Image GetImage(String name){
-			if(imageSet.ContainsKey(name))
+		public Image GetImage(String name) {
+			if (imageSet.ContainsKey(name))
 				return imageSet[name];
-			else
-			{
+			else {
 				Image myImage = new Image("content/" + name + ".png");
 				imageSet.Add(name, myImage);
 				return myImage;
-			}
-				
-				
+			}				
 		}
 	}
 }
