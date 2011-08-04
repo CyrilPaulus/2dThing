1bec273b1d8ecd7fcd80ca0725b09140854bd377
Cyril Paulus <paulus.cyril@gmail.com>
2011-08-05 00:09:25 +0200

    VectorUtils code cleaning

--- 2dThing/Utils/VectorUtils.cs
+++ 2dThing/Utils/VectorUtils.cs
@@ -4,20 +4,16 @@ using System.Linq;
 using System.Text;
 using SFML.Window;
 
-namespace _2dThing.Utils
-{
-	class VectorUtils
-	{
-		public static float Distance (Vector2f ori, Vector2f dest)
-		{
+namespace _2dThing.Utils {
+	class VectorUtils {
+		public static float Distance(Vector2f ori, Vector2f dest) {
 			Vector2f diff = dest - ori;
-			return (float)Math.Sqrt (diff.X * diff.X + diff.Y * diff.Y);
+			return (float)Math.Sqrt(diff.X * diff.X + diff.Y * diff.Y);
 		}
 
-		public static Vector2f Normalize (Vector2f ori)
-		{
-			float dist = Distance (new Vector2f (0, 0), ori);
-			return new Vector2f (ori.X / dist, ori.Y / dist);
+		public static Vector2f Normalize(Vector2f ori) {
+			float dist = Distance(new Vector2f(0, 0), ori);
+			return new Vector2f(ori.X / dist, ori.Y / dist);
 		}
 	}
 }
